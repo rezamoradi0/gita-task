7c8a2f2450b6a4d7d3a93f3b50311be02c8c1bc9
rezamoradi0 <REZAMORADI0933@GMAIL.COM>
2024-03-08 21:43:28 +0330

    Accordion Updated

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,39 +1,55 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 function Accordion({ header, children }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  const accordionRef = useRef(null);
   const bodyRef = useRef(null);
   const headerRef = useRef(null);
 
   const [maxHeight, setMaxHeight] = useState("fit-content");
+  const [closeHeight, setCloseHeight] = useState(null);
+  const [openHeight, setOpenHeight] = useState(null);
 
   useEffect(() => {
-    if (isOpen) {
-      const headerHeight = headerRef.current.offsetHeight;
-      const bodyHeight = bodyRef.current.offsetHeight;
-      setMaxHeight(headerHeight + bodyHeight + "px");
-    } else {
-      const headerHeight = headerRef.current.offsetHeight;
-      setMaxHeight(headerHeight + "px");
-    }
+    isOpen ? setMaxHeight(openHeight) : setMaxHeight(closeHeight);
   }, [isOpen]);
+
+  useEffect(() => {
+    const accordionPaddingBottom = +window
+      .getComputedStyle(accordionRef.current)
+      .getPropertyValue("padding-bottom")
+      .replace("px", "");
+    const accordionPaddingTop = +window
+      .getComputedStyle(accordionRef.current)
+      .getPropertyValue("padding-top")
+      .replace("px", "");
+    const accordionPaddingY = accordionPaddingBottom + accordionPaddingTop;
+    const headerHeight = headerRef.current.offsetHeight;
+    const bodyHeight = bodyRef.current.offsetHeight;
+
+    setCloseHeight(headerHeight + accordionPaddingY + "px");
+    setOpenHeight(headerHeight + bodyHeight + accordionPaddingY + "px");
+  }, [header, children]);
+  console.log(maxHeight);
   return (
     <div
       style={{ maxHeight: maxHeight }}
-      className="overflow-hidden border-red-500 border-2 dark:border-red-500 transition-all duration-500"
+      className="overflow-hidden px-4 pt-2 pb-3  border-2 dark:border-secondary-dark transition-all duration-500"
+      ref={accordionRef}
     >
-      <div ref={headerRef}>
+      <div
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        className="flex"
+        ref={headerRef}
+      >
         {header}{" "}
-        <button
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
-          test
-        </button>
       </div>
-      <div ref={bodyRef}>{children} </div>
+      <div className="p-4" ref={bodyRef}>
+        {children}{" "}
+      </div>
     </div>
   );
 }
