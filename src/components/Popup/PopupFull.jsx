609631c0979883fd49f6b8d3f221ff5da7f9e397
rezamoradi0 <REZAMORADI0933@GMAIL.COM>
2024-03-09 20:08:38 +0330

    Popup mini added ,Popup Full overflow hided

--- src/components/Popup/PopupFull.jsx
+++ src/components/Popup/PopupFull.jsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button";
 function PopupFull({ children, closeFun, headerText }) {
   return createPortal(
     <div className="absolute left-0 top-0 flex h-screen w-screen flex-col items-center justify-center overflow-hidden">
-      <div className="fixed left-1/2  top-1/2 z-50 h-fit w-max -translate-x-1/2  -translate-y-1/2 flex-col  justify-center rounded-lg  border p-16  dark:bg-primary-dark dark:text-secondary-dark">
+      <div className="fixed left-1/2  overflow-hidden top-1/2 z-50 h-fit w-max -translate-x-1/2  -translate-y-1/2 flex-col  justify-center rounded-lg  border p-16  dark:bg-primary-dark dark:text-secondary-dark">
       <Button
             onClick={closeFun}
             className="fa-sharp fa-regular fa-circle-xmark mx-0 border-none rounded-full text-2xl absolute left-0 top-0"
---
+++ src/components/Popup/PopupMini.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+import { twMerge } from "tailwind-merge";
+
+function PopupMini({time=1500,className="",closeFun,text}) {
+    useEffect(()=>{
+        const timeout=setTimeout(()=>{
+            closeFun();
+            console.log("HERE");
+        },time)
+
+        return ()=>{
+            clearTimeout(timeout)
+        };
+    },[])
+    return (
+        <div className={twMerge("absolute top-0 p-4 bg-secondary-dark text-primary-dark  right-2  border animate-hideAndSeek",className)}>
+      {text}
+        </div>
+    )
+}
+
+export default PopupMini
